57fd1b10b1f8edeb2414ebfcfe88469160985891
2302022 <mc.chen@iisigroup.com>
2024-05-23 15:44:41 +0800

    順序區間變成10 之後比較容易擴充

--- clean-base-parent/clean-base-internal-gateway/src/main/java/com/redhat/cleanbase/constant/OrderConstant.java
+++ clean-base-parent/clean-base-internal-gateway/src/main/java/com/redhat/cleanbase/constant/OrderConstant.java
@@ -11,10 +11,10 @@ public final class OrderConstant {
 
     public static final int EXCEPTION_FILTER_ORDER = Ordered.HIGHEST_PRECEDENCE;
 
-    public static final int API_SWITCH_FILTER_ORDER = HIGHEST_PRECEDENCE_500;
-    public static final int DUPLICATED_REQUEST_FILTER_ORDER = HIGHEST_PRECEDENCE_500 + 1;
-    public static final int AUTH_FILTER_ORDER = HIGHEST_PRECEDENCE_500 + 2;
-    public static final int VERSION_CONTROL_FILTER_ORDER = HIGHEST_PRECEDENCE_500 + 3;
-    public static final int DEVICE_ID_FILTER_ORDER = HIGHEST_PRECEDENCE_500 + 4;
-    public static final int CIPHER_FILTER_ORDER = HIGHEST_PRECEDENCE_500 + 5;
+    public static final int API_SWITCH_FILTER_ORDER = HIGHEST_PRECEDENCE_500 + 10;
+    public static final int DUPLICATED_REQUEST_FILTER_ORDER = HIGHEST_PRECEDENCE_500 + 20;
+    public static final int AUTH_FILTER_ORDER = HIGHEST_PRECEDENCE_500 + 30;
+    public static final int VERSION_CONTROL_FILTER_ORDER = HIGHEST_PRECEDENCE_500 + 40;
+    public static final int DEVICE_ID_FILTER_ORDER = HIGHEST_PRECEDENCE_500 + 50;
+    public static final int CIPHER_FILTER_ORDER = HIGHEST_PRECEDENCE_500 + 60;
 }
