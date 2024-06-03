eeceb091bbdcdd0234fbbfeaad82a0292d9a9e74
ming <zxc81906119@gmail.com>
2024-06-03 16:42:29 +0800

    整成同一包

--- clean-base-parent/clean-base-internal-gateway/src/main/java/com/redhat/cleanbase/exception/handler/ExceptionHandler.java
+++
@@ -1,8 +0,0 @@
-package com.redhat.cleanbase.exception.handler;
-
-import org.springframework.web.server.ServerWebExchange;
-import reactor.core.publisher.Mono;
-
-public interface ExceptionHandler<T extends Throwable> {
-    Mono<Void> process(ServerWebExchange exchange, T throwable);
-}
