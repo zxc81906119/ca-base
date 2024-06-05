7debecbffcb182f5433174dadcc63622d0d633a4
ming <zxc81906119@gmail.com>
2024-06-05 13:17:54 +0800

    刪除 redisson 套件 以及 將測試類放入

---
+++ clean-base-parent/clean-base-internal-gateway/src/test/java/com/redhat/cleanbase/filter/ApiSwitchFilterTest.java
@@ -0,0 +1,13 @@
+package com.redhat.cleanbase.filter;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ApiSwitchFilterTest {
+
+    @Test
+    public void filter_happy_flow() {
+        Assertions.assertTrue(true);
+    }
+
+}
