d2a7387dbb9458e064ba88be5056f9724226ad28
ming <zxc81906119@gmail.com>
2024-06-08 10:34:49 +0800

    更改寫入 response 作法

--- clean-base-parent/clean-base-internal-gateway/src/main/java/com/redhat/cleanbase/exception/handler/ExampleExceptionHandler.java
+++ clean-base-parent/clean-base-internal-gateway/src/main/java/com/redhat/cleanbase/exception/handler/ExampleExceptionHandler.java
@@ -1,22 +1,25 @@
 package com.redhat.cleanbase.exception.handler;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.redhat.cleanbase.exception.ExampleException;
-import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
+import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.core.io.buffer.DefaultDataBufferFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
-import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @Component
 public class ExampleExceptionHandler implements ExceptionHandler<ExampleException> {
-    @SneakyThrows
+
+    public static final String ERROR_MESSAGE = "errorMessage";
+
     @Override
     public Mono<Void> process(ServerWebExchange exchange, ExampleException throwable) {
         log.info("do nothing for example exception");
@@ -25,16 +28,24 @@ public class ExampleExceptionHandler implements ExceptionHandler<ExampleExceptio
 
         response.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR);
 
-        val dataBufferFactory = new DefaultDataBufferFactory();
+        val message = throwable.getMessage();
 
-        val responseMap = new HashMap<String, String>();
-        responseMap.put("message", throwable.getMessage());
+        val data = Map.of(ERROR_MESSAGE, message);
 
-        val responseData = new ObjectMapper()
-                .writeValueAsBytes(responseMap);
-
-        val dataBuffer = dataBufferFactory.wrap(responseData);
-
-        return response.writeWith(Mono.just(dataBuffer));
+        try {
+            val dataBuffer = getResponseDataBuffer(data);
+            return response.writeWith(dataBuffer);
+        } catch (JsonProcessingException e) {
+            log.error("transfer to response data failed", e);
+            return Mono.error(e);
+        }
     }
+
+    public static <T> Mono<DataBuffer> getResponseDataBuffer(T data) throws JsonProcessingException {
+        val dataBufferFactory = new DefaultDataBufferFactory();
+        val responseData = new ObjectMapper().writeValueAsBytes(data);
+        val dataBuffer = dataBufferFactory.wrap(responseData);
+        return Mono.just(dataBuffer);
+    }
+
 }
