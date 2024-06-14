6f658538ae24473381780ec3aa2f7cd5277dce96
ming <zxc81906119@gmail.com>
2024-06-14 09:53:48 +0800

    將其他東東放上

--- clean-base-parent/clean-base-services/clean-base-services-account/src/main/java/com/redhat/cleanbase/common/RedissonDataComponent.java
+++ clean-base-parent/clean-base-services/clean-base-services-account/src/main/java/com/redhat/cleanbase/common/RedissonDataComponent.java
@@ -1,8 +1,10 @@
 package com.redhat.cleanbase.common;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class RedissonDataComponent {
 
 }
