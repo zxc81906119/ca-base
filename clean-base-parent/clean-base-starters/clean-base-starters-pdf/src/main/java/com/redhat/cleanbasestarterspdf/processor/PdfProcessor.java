72e2d7308f8dd26edd73a6eea16a3296d5bfb010
ming <zxc81906119@gmail.com>
2024-06-17 09:54:38 +0800

    刪除無用的方法

--- clean-base-parent/clean-base-starters/clean-base-starters-pdf/src/main/java/com/redhat/cleanbasestarterspdf/processor/PdfProcessor.java
+++ clean-base-parent/clean-base-starters/clean-base-starters-pdf/src/main/java/com/redhat/cleanbasestarterspdf/processor/PdfProcessor.java
@@ -3,7 +3,6 @@ package com.redhat.cleanbasestarterspdf.processor;
 import com.lowagie.text.Document;
 import com.lowagie.text.html.simpleparser.HTMLWorker;
 import com.lowagie.text.pdf.PdfWriter;
-import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 
@@ -26,11 +25,6 @@ public class PdfProcessor {
     public void htmlToPdf(File pdfFile, String html) throws IOException {
         val pdfOutputStream = new FileOutputStream(pdfFile, false);
         htmlToPdf(new StringReader(html), pdfOutputStream);
-    }
-
-    @SneakyThrows
-    public void htmlToPdfOrThrows(File pdfFile, String html) {
-        htmlToPdf(pdfFile, html);
     }
 
 }
